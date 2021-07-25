afadf29a7cfcd8492edc2b0818d735df9840fb69
Paul Adams <ghusiyy@gmail.com>
2021-07-25 00:47:49 -0700

    impl for gameboard

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
+  const gameboard = [...Array(7)].map((x) => Array(6).fill(""));
+  console.log(gameboard);
 </script>
 
 <main>
   <h1>Connect Four</h1>
-  <div class="gameboard" />
+  <div class="gameboard">
+    {#each gameboard as col, i}
+      <div class="col">
+        {#each col as row, j}
+          <div class="cell">
+            <p>{row}</p>
+          </div>
+        {/each}
+      </div>
+    {/each}
+  </div>
 </main>
 
 <style>
@@ -12,19 +24,29 @@
       Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   }
 
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
   .gameboard {
     height: 400px;
     width: 400px;
-    border: 1px solid black;
+    background-color: #516dad;
     margin: 0 auto;
     display: grid;
+    grid-template-columns: repeat(7, auto);
+    grid-template-rows: repeat(6, auto);
+    column-gap: 10px;
+    row-gap: 10px;
+    padding: 12px;
+  }
+
+  .cell {
+    height: 48px;
+    background-color: white;
+    border-radius: 100%;
+  }
+  .cell p {
+    vertical-align: middle;
   }
   h1 {
+    text-align: center;
     color: cornflowerblue;
     text-transform: uppercase;
     font-size: 3rem;
